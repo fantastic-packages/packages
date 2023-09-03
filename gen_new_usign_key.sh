3f5b9d7b1dae01c1e94a736465cb39734c4fb106
Anya Lin <hukk1996@gmail.com>
2023-09-03 17:43:20 +0800

    update usign script

--- gen_new_usign_key.sh
+++ gen_new_usign_key.sh
@@ -2,6 +2,6 @@
 USIGN="./.host/x86_64/bin/usign"
 
 #$USIGN -G -p >(cat) -s >(cat)
-$USIGN -G -p keys/usign/usign.pub -s keys/usign/usign.sec
+$USIGN -G -p keys/usign/usign.pub -s keys/usign/usign.sec -c "Public usign key for fantastic-packages builds"
 mv keys/usign/usign.pub keys/usign/$($USIGN -F -p keys/usign/usign.pub|tr 'a-z' 'A-Z').pub
 mv keys/usign/usign.sec keys/usign/$($USIGN -F -s keys/usign/usign.sec|tr 'a-z' 'A-Z').sec
