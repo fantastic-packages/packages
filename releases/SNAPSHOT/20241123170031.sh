5bf5ca521b1c51ab14a2abc2886459cecb6e7da3
GitHub Action <actions-user@users.noreply.github.com>
2024-11-23 16:14:14 +0000

    Update packages

---
+++ releases/SNAPSHOT/20241123170031.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+KEYID=20241123170031
+mkdir -p /etc/apk/keys 2>/dev/null
+cat <<- PUBKEY > /etc/apk/keys/fantastic-packages-${KEYID}.pem
+-----BEGIN PUBLIC KEY-----
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEP1bBDT4R1QuhUWY5qqI1FNQ0rshE
+Oh51xeVcLaZoYkUWNTBUtlTQNGsWIcFFn66cun4DFY1kLTzF8k5V140ZXg==
+-----END PUBLIC KEY-----
+
+PUBKEY
