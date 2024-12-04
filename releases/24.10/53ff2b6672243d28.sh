3f1dbfb139f70018acfc2111b708d47906cdcbe9
Anya Lin <hukk1996@gmail.com>
2024-12-04 19:14:41 +0800

    Update packages

---
+++ releases/24.10/53ff2b6672243d28.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+KEYID=53ff2b6672243d28
+mkdir -p /etc/opkg/keys 2>/dev/null
+cat <<- PUBKEY > /etc/opkg/keys/${KEYID}
+untrusted comment: Public usign key for fantastic-packages builds
+RWRT/ytmciQ9KFjHE8DQ97piX7oHvdr49H3VLlJDuJNmuaKFguOrwXBG
+PUBKEY
